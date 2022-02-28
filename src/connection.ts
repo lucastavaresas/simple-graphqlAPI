c274d6e693441a19e597b8dc926c34c806f9b265
Lucas Tavares <lucastavaresas@gmail.com>
2022-02-28 15:22:37 -0300

    Changes in mongo

--- src/connection.ts
+++ src/connection.ts
@@ -1,6 +1,6 @@
 import {connect} from "mongoose";
 
-connect("mongodb+srv://imunelabs:dyyJk3cRJ6zatBuw@imunelabs.nt1mc.mongodb.net/gra?retryWrites=true&w=majority")
+connect("mongodb://localhost:27017/gra?retryWrites=true&w=majority")
   .then(() => {
     console.log('MongoDb is running')
   }).catch(err=> console.log(err))
